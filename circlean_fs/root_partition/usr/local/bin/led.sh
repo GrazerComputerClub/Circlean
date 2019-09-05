4809349dc4df2a8566d68afcdfb4716951ed8fb7
mstroh76 <m.stroh76@gmail.com>
2019-09-05 22:32:13 +0200

    handle events to change led state

--- circlean_fs/root_partition/usr/local/bin/led.sh
+++ circlean_fs/root_partition/usr/local/bin/led.sh
@@ -1,5 +1,59 @@
 #!/bin/bash
 
-#activate or deactivate (parameter 2) output/LED GPIO (parameter 1)
-gpio -g mode $1 out
-gpio -g write $1 $2
+seton(){
+        gpio -g mode $1 out
+        gpio -g write $1 1
+}
+
+setoff(){
+        gpio -g mode $1 out
+        gpio -g write $1 0 
+}
+
+setglow(){
+        gpio -g mode $1 up
+        gpio -g mode $1 in
+}
+
+
+setinput(){
+        gpio -g mode $1 down
+        gpio -g mode $1 in
+}
+
+process_state(){
+
+  case $2 in
+     connected)
+          echo "USB-Drive connected"
+          seton $1
+          ;;
+     disconnected)
+          echo "USB-Drive disconnected"
+          setoff $1
+          ;;
+     mounted)
+          echo "USB-Drive mounted"
+          ;; 
+     unmounted)
+          echo "USB-Drive unmounted"
+          setglow $1
+          ;; 
+     *)
+          echo "error: state $2 not supported"
+          ;;
+  esac
+}
+
+ case $1 in
+     badusb)
+          echo -n "bad port: "
+          process_state 16 $2
+          ;;
+     goodusb)
+          echo -n "good port: "
+          process_state 21 $2
+          ;;
+     *)
+          echo "error: port not supported (use: $0 goodusb connected)"
+  esac
