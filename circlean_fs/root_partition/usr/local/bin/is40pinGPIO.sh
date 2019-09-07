78ff22ba397a156706ae806daef4061e0b711da9
mstroh <m.stroh@ymail.com>
2019-09-07 12:04:13 +0200

    Reduce cpu load for Pi 1 and Zero

--- circlean_fs/root_partition/usr/local/bin/is40pinGPIO.sh
+++ circlean_fs/root_partition/usr/local/bin/is40pinGPIO.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#https://www.raspberrypi-spy.co.uk/2012/09/checking-your-raspberry-pi-board-version/
 Revision=`cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}'`
 RevisionDec=`echo $(( 16#$Revision ))`
-echo -n "Revision: ${RevisionDec}, "
+echo -n "CPU Revision: ${RevisionDec}, "
 if [ $RevisionDec -ge 16 ]; then
         echo 40-pin GPIOs
         exit 0
